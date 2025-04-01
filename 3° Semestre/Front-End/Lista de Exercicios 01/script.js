81f6ff0caf2063860d555d62f4eb0a30ef9df4be
unknown <leonardotrx@outlook.com>
2025-04-01 18:04:18 -0300

    Execução de exercicios 01-04-2025

---
+++ 3° Semestre/Front-End/Lista de Exercicios 01/script.js
@@ -0,0 +1,35 @@
+// alert("Olá Mundo")
+
+// console.log("Meu primeiro Script em JavaScript!")
+
+// let num1 = 5, num2 = 10, soma = num1 + num2
+// console.log(soma)
+
+// let nome = prompt("Informe o seu nome: ");
+// alert("Olá "+nome+" Seja bem vindo!");
+
+// let string = "29", numero = parseInt(string)
+// console.log(numero)
+
+// let variavel = true;
+// console.log(variavel);
+
+// let type = "29";
+// console.log(typeof type)
+
+// let n1 = Number(prompt("Informe o primeiro numero: ")), n2 = Number(prompt("Informe o segundo numero: ")), soma = n1 + n2;
+// alert("A soma dos numeros deu: "+ soma)
+
+// let valor = 29;
+// console.log("Valor antes da mudança: "+ valor)
+// valor = 58;
+// console.log("Valor após a mudança: "+ valor)
+
+// let numero = Number(prompt("Digite o numero: "));
+
+// if(numero % 2 === 0){
+//     console.log("O numero informado "+ numero +" é par!")
+// } else {
+//     console.log("O numero informado "+ numero +" é impar!")
+// }
+
