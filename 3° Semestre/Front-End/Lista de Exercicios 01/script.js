143fed126aca8978087cea0fd4ebfa9558d9a72f
Leo Teixeira <167496565+Teeixeira@users.noreply.github.com>
2025-04-01 22:27:02 -0300

    resolução de novos exericicios 01-04-2025

--- 3° Semestre/Front-End/Lista de Exercicios 01/script.js
+++ 3° Semestre/Front-End/Lista de Exercicios 01/script.js
@@ -33,3 +33,46 @@
 //     console.log("O numero informado "+ numero +" é impar!")
 // }
 
+// let i;
+// for(i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// const meunumero = 10
+// console.log(meunumero);
+// meunumero = 20
+// console.log(meunumero);
+
+// let meunumero = 10
+// console.log(meunumero);
+// meunumero = 20
+// console.log(meunumero);
+
+// let num1 = (Number(prompt("Insira o primeiro numero!"))), num2 = (Number(prompt("Insira o segundo numero!"))), num3 = (Number(prompt("Insira o terceiro numero!")));
+// let media = (num1 + num2 + num3) / 3
+// console.log(media)
+
+// let num1 = (Number(prompt("Insira o numero que sera dobrado!"))), dobro = num1 * 2
+// console.log(dobro)
+
+// let c, f;
+// c = Number(prompt("Qual a temperatura atual?"))
+// f = 32 + (c * 9/5)
+// console.log(f)
+
+// let idade = Number(prompt("Informe a sua idade:"))
+// if(idade >= 18){
+//     console.log("Usuário é maior de idade!")
+// } else {
+//     console.log("Usuário é menor de idade")
+// }
+
+// let num1 = (Number(prompt("Insira o primeiro numero!"))), num2 = (Number(prompt("Insira o segundo numero!"))), num3 = (Number(prompt("Insira o terceiro numero!")))
+// let maior = num1
+// if(num2 > maior){
+//     maior = num2
+// } 
+// if (num3 > maior){
+//     maior = num3
+// }
+// console.log("Maior numero: "+maior)
