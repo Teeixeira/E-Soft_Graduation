4c901b3e4029a0fa117d65a430fc93ddc2aae82d
Leo Teixeira <167496565+Teeixeira@users.noreply.github.com>
2025-04-07 22:10:11 -0300

    Finalização da atividade 5

--- 3° Semestre/Front-End/5° Aividade/script.js
+++ 3° Semestre/Front-End/5° Aividade/script.js
@@ -16,4 +16,24 @@ document.getElementById('btnAdicionar').addEventListener('click', function() {
     adicionarPessoa(nome, idade, sexo)
 
     console.log(pessoas)
-})
+    
+});
+
+document.getElementById('verificarItem').addEventListener('click',function (){
+    const nome = document.getElementById('buscarNome').value;
+    
+    for(let i = 0; i < pessoas.length; i ++){
+        if (pessoas[i].nome === nome) {
+            alert("Usuário encontrado: "+nome+", índice: "+i);
+            return;
+        }
+    }
+    alert("Usuário não encontrado!");
+});
+
+document.getElementById('removerItem').addEventListener('click', function (){
+    const indexRemover = parseInt(document.getElementById('buscarIndex').value);
+
+    pessoas.splice(indexRemover, 1)
+    console.log(pessoas)
+});
