fb417f01e82e5f75d52f444dec64840a582dce85
unknown <leonardotrx@outlook.com>
2025-04-15 16:09:11 -0300

    execução de exercicios 15/04/2025

--- 3° Semestre/Estrutura de Dados/Lista de Exercícios/025.c
+++ 3° Semestre/Estrutura de Dados/Lista de Exercícios/025.c
@@ -1,4 +1,5 @@
-/*Construa um programa em linguagem C que solicite 4 palavras com no máximo 10 caracteres (armazenando-as em um vetor)  e posteriormente apresente: 
+/*Construa um programa em linguagem C que solicite 4 palavras com no máximo 10 caracteres 
+(armazenando-as em um vetor)  e posteriormente apresente: 
 a)  O tamanho do vetor (em bytes); 
 b)  A quantidade de caracteres de cada elemento do vetor; 
 c) O endereço de memória do vetor; 
@@ -8,17 +9,27 @@ d)  O endereço de memória de cada elemento do vetor*/
 #include <stdlib.h>
 #include <string.h>
 
-int n[5], *ptr = &n;
+char palavras[4][10];
 
 int main() {
 
-    for (int i = 0; i < 5; i++) {
-        printf("Digite o %d valor de n: ", i + 1);
-        scanf("%d", &n[i]);
+    for(int i = 0; i < 4; i ++){
+        printf("Informe a %d palavra (max 10 carac): ", i + 1);
+        gets(palavras[i]);
     }
 
-    for (int i = 0; i < 5; i++) {
-        printf("\n%d valor do vetor: %d", i + 1, *(n + i));
+    for(int i = 0; i < 4; i ++){
+        int count = 0;
+        for(int j = 0; j < 10; j ++){
+            if(palavras[i][j] != '\0'){
+                count ++;
+            } else {
+                break;
+            }
+            printf("\nEndereco de memoria do %d vetor e %d elemento: %p",j + 1, i + 1, &*(*(palavras + i) + j));
+        }
+        printf("\nA %d palavra tem %d caracteres!", i + 1, count);
+        printf("\nEndereco de memoria do %d vetor: %p", i + 1, &*(palavras + i));
     }
 
     return 0;
